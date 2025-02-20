f554172e7dca36f7d1f6a979751cfdf0a1bb3cf4
Vinay Pal Singh <159277832+VinayPalSingh08@users.noreply.github.com>
2025-02-20 18:56:11 +0530

    dsa

---
+++ dsa35.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+bool isPrime(int n){
+    for(int i=2;i<n;i++){
+        if(n%i==0){
+            return 0;
+        }
+    }
+    return 1;
+}
+int main(){
+int num;
+cin>>num;
+if(isPrime(num)){
+    cout<<"it is not a prime number "<<endl;
+
+}
+else{
+    cout<<"Prime number ";
+}
+}
---
+++ dsa36.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[3]={1,2,3};
+    cout<<"printing the index value "<<endl<<arr[1];
+    return 0;
+
+}
